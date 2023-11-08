279d793e2c50fcd0c6ecfa3b75d839f6e12d1ab8
dhruv-pandit <78376173+dhruv-pandit@users.noreply.github.com>
2023-11-08 11:54:35 +0000

    updates

--- with_africa/dashboard/V1/eoa-dashboard-V1.0.8.py
+++ with_africa/dashboard/V1/eoa-dashboard-V1.0.8.py
@@ -24,14 +24,7 @@ st.markdown(streamlit_style, unsafe_allow_html=True)
 #st.set_page_config(layout="wide")
 
 @st.cache_data
-def load_data():
-    url = 'https://github.com/dhruv-pandit/Projects/raw/main/with_africa/with_eoa_copy.xlsx'
-
-    df = pd.read_excel(url, sheet_name = 'eoa_dashboard_data').rename(columns={'ISO3 Code' : 'ISO3_Code', 'Ease of Access' : 'EOA_Score'})
-
-    return df
 def load_data_new():
-    
     url = 'https://github.com/dhruv-pandit/Projects/raw/main/with_africa/dashboard/results.xlsx'
     df_org_results = pd.read_excel(url, sheet_name='organisations')
     df_ngos_results = pd.read_excel(url, sheet_name='ngos')
@@ -49,49 +42,7 @@ def load_tables():
 
 
 df_infotext, df_scorelegend = load_tables()
-df_eoa = load_data()
 df_gov_results, df_ngos_results, df_org_results = load_data_new()
-# Adjusting the function to remove the specific information and focus on standout aspects and regional comparison
-def create_summary_comment(row, region_stats):
-    # Determine standout features
-    standout_features = []
-    if row['year_score'] == 5:
-        standout_features.append("very recent data")
-    elif row['year_score'] == 0:
-        standout_features.append("outdated data")
-    
-    if row['indic_score'] == 5:
-        standout_features.append("a full range of indicators")
-    elif row['indic_score'] == 0:
-        standout_features.append("very few indicators")
-    
-    if row['security_warning'] == 0:
-        standout_features.append("security concerns")
-    
-    if row['odfa_star'] == 1:
-        standout_features.append("data exclusive to ODFA")
-    
-    # Check if country has the highest or lowest final score within its region
-    regional_comparison = ""
-    if row['final_score'] == region_stats['max'][row['region']]:
-        regional_comparison = "top accessibility score in its region"
-    elif row['final_score'] == region_stats['min'][row['region']]:
-        regional_comparison = "lowest accessibility score in its region"
-    elif row['final_score'] > region_stats['mean'][row['region']]:
-        regional_comparison = "above-average accessibility score in its region"
-    else:
-        regional_comparison = "below-average accessibility score in its region"
-    
-    # Combine standout features and regional comparison
-    comment = f"{row['country']} stands out for {' and '.join(standout_features)} and has {regional_comparison}."
-    
-    # Limit the comment to 50 words
-    return ' '.join(comment.split()[:50])
-
-
-region_stats = df_gov_results.groupby('region')['final_score'].agg(['mean', 'max', 'min']).to_dict()
-# Apply the function to each row in the DataFrame to create the summaries
-df_gov_results['comments'] = df_gov_results.apply(lambda row: create_summary_comment(row, region_stats), axis=1)
 
 st.title("African Tourism Data Accessibility (2023)")
 st.write("""
@@ -106,14 +57,14 @@ col1, col2 = st.columns(2)
 
 with col1:
     option = st.selectbox(
-    'Select an Indicator',options = ['Recency', 'Ease of Access Score', 'Machine Readability Data Available', 'Number of Indicators'])
+    'Select an Indicator',options = ['Ease of Access Score', 'Recency of Data', 'Machine Readability Data Available', 'Number of Indicators'])
     st.write(f"""
     ## {option}
     ____
     """)
 
     dic = {
-        'Recency' : 'recency',
+        'Recency of Data' : 'recency',
         'Ease of Access Score': 'final_score', 
         'Machine Readability Data Available' : 'machine_read', 
         'Number of Indicators' : 'num_indicators'
